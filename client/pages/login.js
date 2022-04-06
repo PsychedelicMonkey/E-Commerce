6b328661a5047ad981fbc57ec589315d4473f73e
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-04-06 10:48:47 -0600

    Login page

---
+++ client/pages/login.js
@@ -0,0 +1,58 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    alert(JSON.stringify({ email, password }));
+  };
+
+  return (
+    <div className="form-center">
+      <Head>
+        <title>Log In</title>
+      </Head>
+
+      <div className="auth-form">
+        <h1>Log In</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="form-input">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="form-input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Log In</button>
+        </form>
+
+        <span className="register-link">
+          Don't have an account? <Link href="#">Register</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
