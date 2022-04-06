94a32ebfc334ab06fdb1d2d17b4e6ef47bf3e568
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2022-04-06 16:52:35 -0600

    Cart styles

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -7,59 +7,73 @@ import Link from 'next/link';
 const Cart = () => {
   const { items, removeFromCart } = useContext(CartContext);
 
+  const getTotal = () => {
+    let total = 0.0;
+    items.map((item) => {
+      total += item.price;
+    });
+
+    return parseFloat(total).toFixed(2);
+  };
+
   return (
     <div className="container">
       <Head>
         <title>Cart - ECommerce</title>
       </Head>
 
-      <section>
-        {items.length > 0 ? (
-          <>
-            <h1>My Cart</h1>
+      {items.length > 0 ? (
+        <section className="table-wrap">
+          <h1>My Cart</h1>
 
-            <table>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th></th>
+          <table className="cart-table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Image
+                      src={`http://localhost:1337${item.image.url}`}
+                      width={item.image.width}
+                      height={item.image.height}
+                    />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>${item.price}</td>
+                  <td>
+                    <button
+                      className="sm red"
+                      onClick={removeFromCart.bind(this, item.id)}
+                    >
+                      Remove
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {items.map((item) => (
-                  <tr key={item.id}>
-                    <td>
-                      <Image
-                        src={`http://localhost:1337${item.image.url}`}
-                        width={item.image.width}
-                        height={item.image.height}
-                      />
-                    </td>
-                    <td>{item.name}</td>
-                    <td>${item.price}</td>
-                    <td>
-                      <button
-                        className="sm red"
-                        onClick={removeFromCart.bind(this, item.id)}
-                      >
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+              ))}
+            </tbody>
+          </table>
 
-            <Link href="#">
-              <a className="button">Proceed to checkout</a>
-            </Link>
-          </>
-        ) : (
+          <h3 className="total">Order total: ${getTotal()}</h3>
+
+          <Link href="#">
+            <a id="checkout-btn" className="button">
+              Proceed to checkout
+            </a>
+          </Link>
+        </section>
+      ) : (
+        <div className="empty-text">
           <h3 className="empty">Your cart is empty</h3>
-        )}
-      </section>
+          <Link href="/">Back to home</Link>
+        </div>
+      )}
     </div>
   );
 };
